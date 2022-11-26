772117c2171322270f3a44abcbce71fa7b3f77a5
Andrew Wolven <awolven@gmail.com>
2022-11-26 11:46:03 -0600

    updated to application-font instead of *font*

--- src/kernel/main.lisp
+++ src/kernel/main.lisp
@@ -112,7 +112,7 @@ krma::(defmethod main ((app kons-9::kons-9))
           ;; these should be put in initialize-instance of krma-test-application
           (multiple-value-bind (width height) (get-framebuffer-size main-window)
             (setf (main-window-width app) width
-	              (main-window-height app) height))
+	          (main-window-height app) height))
 
           kons-9::
           (progn
@@ -145,24 +145,20 @@ krma::(defmethod main ((app kons-9::kons-9))
           (reset-command-pool device command-pool)
 
           ;; one time commands here.
-          (unless (probe-file (asdf/system:system-relative-pathname :krma "acache.json"))
-            (sdf-bmfont:create-bmfont #+linux "/usr/share/fonts/liberation-mono/LiberationMono-Regular.ttf"
-				                      #+darwin "/System/Library/Fonts/Monaco.ttf"
-                                      #+windows "C:/Windows/Fonts/Arial.ttf" "acache.json"
-                                      :size 16 :mode :msdf+a :type :json :spread 8))
-          #-(or linux darwin)
-          (unless (probe-file (asdf/system:system-relative-pathname :krma "tcache.json"))
-            (sdf-bmfont:create-bmfont "C:/Windows/Fonts/Times.ttf" "tcache.json"
-                                      :size 16 :mode :msdf+a :type :json :spread 8))
+	  (unless (probe-file (asdf/system:system-relative-pathname
+			       :krma "submodules/krma-fonts/rm16cache.json"))
+	    (sdf-bmfont:create-bmfont
+	     (asdf/system:system-relative-pathname
+	      :krma "submodules/krma-fonts/Roboto_Mono/static/RobotoMono-Medium.ttf")
+	     (asdf/system:system-relative-pathname :krma "submodules/krma-fonts/rm16cache.json")
+	     :size 16 :mode :msdf+a :type :json :spread 8))
 
-
-          (setq *font*
-                (vulkan-make-font device queue sampler texture-dsl descriptor-pool command-buffer
-                                  :cache-file "acache.json"))
-          #-(or linux darwin)
-          (setq *font2*
-                (vulkan-make-font device queue sampler texture-dsl descriptor-pool command-buffer
-                                  :cache-file "tcache.json"))
+	  (uiop/filesystem:with-current-directory
+	      ((asdf/system:system-relative-pathname :krma "submodules/krma-fonts/"))
+	    (setf (application-default-font *app*)
+		  (vulkan-make-font
+		   device queue sampler texture-dsl descriptor-pool command-buffer
+		   :cache-file "rm16cache.json")))
 
           (let* ((bpp 4)
                  (bitmap (make-array bpp :element-type '(unsigned-byte 8) :initial-element #xff)))
@@ -179,89 +175,89 @@ krma::(defmethod main ((app kons-9::kons-9))
             (with-slots (exit?) app
 
               (loop until (glfw:window-should-close-p)
-		            with frames = 0
-		            with fps
-		            with time = (/ (get-internal-real-time) internal-time-units-per-second)
-		            with base-time = 0
+		    with frames = 0
+		    with fps
+		    with time = (/ (get-internal-real-time) internal-time-units-per-second)
+		    with base-time = 0
                     with last-time = time
-		            do
-		               (glfw:poll-events)
+		    do
+		       (glfw:poll-events)
 
-                       (when (< (- last-time time) #.(/ 120.0f0))
-                         ;; let's not heat up the computer if we're in VK_PRESENT_MODE_IMMEDIATE or something
-                         ;; can't seem to get a resolution lower than 1/60 s or so
-                         ;; oh well, works ok on my PC ~60Hz
-                         ;; nanosleep must call sleep on Windows!
-                         (sb-unix:nanosleep 0 2000000))
+                    #-windows(when (< (- last-time time) #.(/ 120.0f0))
+			       ;; let's not heat up the computer if we're in VK_PRESENT_MODE_IMMEDIATE or something
+			       ;; can't seem to get a resolution lower than 1/60 s or so
+			       ;; oh well, works ok on my PC ~60Hz
+			       ;; nanosleep must be calling sleep on Windows!
+                               (sb-unix:nanosleep 0 2000000))
 
-                       (setq last-time time)
+			     (setq last-time time)
 
-		               (when (recreate-swapchain? main-window)
-		                 (multiple-value-bind (width height) (get-framebuffer-size main-window)
-		                   (recreate-swapchain main-window (swapchain main-window) width height)
-		                   (setf (main-window-width app) width
-			                     (main-window-height app) height)
-		                   (setf (recreate-swapchain? main-window) nil)))
+		             (when (recreate-swapchain? main-window)
+		               (multiple-value-bind (width height) (get-framebuffer-size main-window)
+		                 (recreate-swapchain main-window (swapchain main-window) width height)
+		                 (setf (main-window-width app) width
+			               (main-window-height app) height)
+		                 (setf (recreate-swapchain? main-window) nil)))
 
-		               (let* ((swapchain (swapchain main-window))
-			                  (frame-count (number-of-images swapchain))
-			                  (current-frame (car current-frame-cons))
-			                  (current-draw-data (car current-draw-data-cons))
-			                  (scene (application-scene app))
-			                  (frame-resource0 (elt (frame-resources swapchain) current-frame))
-			                  (command-buffer (frame-command-buffer frame-resource0))
-			                  (rm-draw-data (aref (rm-draw-data scene) current-draw-data)))
+		             (let* ((swapchain (swapchain main-window))
+			            (frame-count (number-of-images swapchain))
+			            (current-frame (car current-frame-cons))
+			            (current-draw-data (car current-draw-data-cons))
+			            (scene (application-scene app))
+			            (frame-resource0 (elt (frame-resources swapchain) current-frame))
+			            (command-buffer (frame-command-buffer frame-resource0))
+			            (rm-draw-data (aref (rm-draw-data scene) current-draw-data)))
 
-		                 (erase-immediate-mode-draw-data app)
-		                 (setq work-queue (draw-data-work-queue rm-draw-data))
+		               (erase-immediate-mode-draw-data app)
+		               (setq work-queue (draw-data-work-queue rm-draw-data))
 
-		                 (maybe-defer-debug (app)
-		                   (loop with work = nil
-			                     while (setq work (sb-concurrency:dequeue work-queue))
-			                     do (funcall work)))
+		               (maybe-defer-debug (app)
+		                 (loop with work = nil
+			               while (setq work (sb-concurrency:dequeue work-queue))
+			               do (funcall work)))
 
-		                 (call-immediate-mode-work-functions app)
+		               (call-immediate-mode-work-functions app)
 
-		                 (setq image-index
-			                   (frame-begin swapchain (render-pass swapchain)
-					                        current-frame (kons-9::c! 1 1 1 1)
-					                        command-pool))
+		               (setq image-index
+			             (frame-begin swapchain (render-pass swapchain)
+					          current-frame (kons-9::c! 1 1 1 1)
+					          command-pool))
 
-                         ;;(update-2d-camera scene)
-                         ;;(update-3d-camera scene)
+			       ;;(update-2d-camera scene)
+			       ;;(update-3d-camera scene)
 
-		                 (maybe-defer-debug (app)
-		                   kons-9::(3d-setup-projection)
-		                   kons-9::(destructuring-bind (w h) (application-window-size *app*)
-				                     (2d-setup-projection w h))
-		                   kons-9::(progn
-				                     (draw-scene-view *scene-view*)
-				                     (update-status-bar-for-scene)
-				                     krma::(incf frames)
-				                     krma::(setq time (/ (get-internal-real-time) internal-time-units-per-second))
-				                     krma::(when (> (- time base-time) 1)
-					                         (setq fps (float (/ frames (- time base-time))))
-					                         (setq base-time time)
-					                         (setq frames 0))
+		               (maybe-defer-debug (app)
+		                 kons-9::(3d-setup-projection)
+		                 kons-9::(destructuring-bind (w h) (application-window-size *app*)
+				           (2d-setup-projection w h))
+		                 kons-9::(progn
+				           (draw-scene-view *scene-view*)
+				           (update-status-bar-for-scene)
+				           krma::(incf frames)
+				           krma::(setq time (/ (get-internal-real-time) internal-time-units-per-second))
+				           krma::(when (> (- time base-time) 1)
+					           (setq fps (float (/ frames (- time base-time))))
+					           (setq base-time time)
+					           (setq frames 0))
 				   
 				     
-				                     (krma::draw-text (format nil "fps: ~4,0f" krma::fps) (- (car (application-window-size krma::app)) 100) 10 :color #x000000ff)
-				                     ))
+				           (krma::draw-text (format nil "fps: ~4,0f" krma::fps) (- (car (application-window-size krma::app)) 100) 10 :color #x000000ff)
+				           ))
 
-                         ;; render here.
-		                 (render-scene scene app command-buffer rm-draw-data (im-draw-data scene))
+			       ;; render here.
+		               (render-scene scene app command-buffer rm-draw-data (im-draw-data scene))
 
 
 
-		                 (frame-end swapchain queue current-frame)
+		               (frame-end swapchain queue current-frame)
 
-		                 (frame-present swapchain queue current-frame image-index main-window)
+		               (frame-present swapchain queue current-frame image-index main-window)
 
-                         ;; this needs to be the only thread that modifies current-frame
-		                 (sb-ext:atomic-update (car current-frame-cons)
-					                           #'(lambda (cf) (mod (1+ cf) frame-count)))
-		                 (sb-ext:atomic-update (car current-draw-data-cons)
-					                           #'(lambda (cdd) (mod (1+ cdd) 2)))))
+			       ;; this needs to be the only thread that modifies current-frame
+		               (sb-ext:atomic-update (car current-frame-cons)
+					             #'(lambda (cf) (mod (1+ cf) frame-count)))
+		               (sb-ext:atomic-update (car current-draw-data-cons)
+					             #'(lambda (cdd) (mod (1+ cdd) 2)))))
 
               (shutdown-application app)))))
 
